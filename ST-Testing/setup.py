1eb5ec8709ce430df4736324f4d3ab041b8e0bc2
Robert Aslan <95583757+Acho441@users.noreply.github.com>
2023-11-21 09:04:36 -0500

    Delete ST-Testing directory

--- ST-Testing/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-   name='ST-Testing',
-   version='1.0',
-   description='code work for the class CSC 485',
-   author='Robert Aslan',
-   packages=['ST-Testing'],
-)
